87eb548367d20757180a2fa34bf5f3dbf94bea57
Fiona Fairlie <f.fairlie@gcu.ac.uk>
2025-11-24 15:19:39 +0000

    updated solution for guestbook features in lab5

--- routes/guestbookRoutes.js
+++ routes/guestbookRoutes.js
@@ -3,36 +3,27 @@ import * as controller from "../controllers/guestbookControllers.js";
 
 const router = express.Router();
 
-// Authentication routes
-router.get("/login", controller.show_login);
-router.post("/login", controller.handle_login);
-
-// Guestbook routes
 router.get("/", controller.static_landing_page);
-// router.get("/", controller.landing_page);
+// router.get('/guestbook', function (req, res) {
+//   res.send('<h1>Guestbook Messages</h1>');
+// })
+router.get('/guestbook', controller.entries_list);
 router.get("/new", controller.show_new_entries);
-router.post("/new", controller.post_new_entry);
-router.get("/posts/:author", controller.show_user_entries);
-
-// User registration routes
+router.get("/login", controller.show_login);
 router.get("/register", controller.show_register_page);
-router.post("/register", controller.post_new_user);
-
-// Logged-in landing page
-router.get("/loggedIn", controller.loggedIn_landing);
-
-// Logout
-router.get("/logout", controller.logout);
 
 // 404 handler
-router.use((req, res) => {
-  res.status(404).render("errors/error", { title: "Page Not Found" });
-});
+router.use(function (req, res) {
+  res.status(404);
+  res.type('text/plain');
+  res.send('404 Not found.');
+})
 
 // Generic error handler
-router.use((err, req, res, next) => {
-  console.error("Unhandled error:", err);
-  res.status(500).render("errors/error", { title: "Internal Server Error" });
-});
+router.use(function (err, req, res, next) {
+  res.status(500);
+  res.type('text/plain');
+  res.send('Internal Server Error.');
+})
 
 export default router;
